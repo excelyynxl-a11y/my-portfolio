47640030c5b43c56f05166b493cea442f592d938
excel <elim0079@student.monash.edu>
2025-11-03 20:20:21 +0800

    setup HomePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme toggle */}
+            <ThemeToggle />
+
+            {/* Background effects */}
+            <StarBackground />
+
+            {/* navbar */} 
+            <Navbar />
+
+            {/* main content */}
+            <main>
+                <HeroSection />
+                <AboutSection />
+            </main>
+
+            {/* footer */}
+
+        </div>
+    );
+};
