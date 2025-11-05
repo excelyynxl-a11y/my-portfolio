de7e9f0a1b495cd41a58cbfb56d74013c28311c7
excel <elim0079@student.monash.edu>
2025-11-05 16:47:24 +0800

    add in resume pdf

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -39,10 +39,10 @@ export const AboutSection = () => {
                             </a>
 
                             <a 
-                                href="" 
+                                href="/excelyynx_resume.pdf"
+                                target="_blank"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
-                                {" "}
                                 View My CV
                             </a>
                         </div>
