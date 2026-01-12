f04a118448f3e2b081fa347c43359dec4655b9b5
excel <excelyynxl@gmail.com>
2026-01-12 10:39:09 +0800

    add KawanKawan project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -20,7 +20,16 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/travel-planner-app",
     },
     {
-        id: 3,
+        id: 4,
+        title: "KawanKawan Social Media App",
+        description: "A SaaS social media platform that allows user authentication, image upload, dynamic voting and commenting system, community groups. The app is designed to be mobile responsive with modern glassmorphism aesthetic effects.",
+        image: "/projects/kawan_kawan_social_media.png",
+        tags: ["Supabase", "React", "TailwindCSS", "GitHub OAuth"],
+        demoUrl: "https://social-media-kawan-kawan-git-main-excelyynxs-projects.vercel.app/",
+        gitUrl: "https://github.com/excelyynxl-a11y/social-media-app",
+    },
+    {
+        id: 5,
         title: "AI Newsletter",
         description: "A SaaS platform that allows users to receive AI-generated newsletters tailored to their interests and preferred frequency.",
         image: "/projects/ai_newsletter.png",
@@ -29,7 +38,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/ai-newsletter",
     },
     {
-        id: 4,
+        id: 6,
         title: "Gemini Clone",
         description: "Cloned Google Gemini webpage using React js and Gemini API integration.",
         image: "/projects/gemini_clone.png",
@@ -38,7 +47,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/gemini-clone",
     },
     {
-        id: 5,
+        id: 7,
         title: "Crypto Dashboard",
         description: "Crypto tracking web application that displays real-time cryptocurrency data, browse top cryptocurrencies, view detailed metrics, and explore a 7-day interactive chart powered by Recharts.",
         image: "/projects/crypto_dashboard.png",
@@ -47,7 +56,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/crypto-price-website",
     },
     {
-        id: 5,
+        id: 8,
         title: "Finance Tracker",
         description: "Finance Tracker with Machine Learning Predictions using Regression, Recharts, Redux Toolkit and Material UI",
         image: "/projects/finance_tracker.png",
@@ -56,7 +65,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/finance-tracker",
     },
     {
-        id: 6,
+        id: 9,
         title: "Weightlifting Management System",
         description: "WLMS that registers athletes, validates for registration of athletes, and determines barbell setup for a liftoff.",
         image: "/projects/wlms.png",
@@ -65,7 +74,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/WeightliftingManagementSystem/tree/main",
     },
     {
-        id: 7,
+        id: 10,
         title: "About Me Quiz",
         description: "Simple 'About Me' quiz create using React framework, Users can answer multiple-choice questions, get instant feedback on correctness, get final score and restart the quiz.",
         image: "/projects/about_me.png",
@@ -74,7 +83,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/about-me-quiz",
     },
     {
-        id: 8,
+        id: 11,
         title: "Pomodoro Timer",
         description: "Pomodoro timer for short, medium and long breaks with sound effect.",
         image: "/projects/pomodoro_timer.png",
@@ -83,7 +92,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/pomodoro.git",
     },
     {
-        id: 9,
+        id: 12,
         title: "Tic Tac Toe",
         description: "Classic tic tac toe game where players can play with computers..",
         image: "/projects/tic_tac_toe.png",
@@ -92,7 +101,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/TicTacToeGame.git",
     },
     {
-        id: 10,
+        id: 13,
         title: "Weather App",
         description: "Weather web that allows users to search a city and obtain a weather forecast.",
         image: "/projects/weather_app.png",
@@ -101,7 +110,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/WeatherApp.git",
     },
     {
-        id: 11,
+        id: 14,
         title: "Text to Voice Converter",
         description: "Text to voice converter where user can input their text, select a voice and hear an audible speech.",
         image: "/projects/text_to_voice_converter.png",
