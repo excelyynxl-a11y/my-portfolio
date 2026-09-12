f2ae49d4e7d44380c0781e38ae761b5499687bfe
excelyynxl-a11y <excelyynxl@gmail.com>
2026-09-12 11:03:01 +0800

    add sapu and volunteerconnect project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -4,7 +4,7 @@ const projects = [
     {
         id: 1,
         title: "Sore High Catalog",
-        description: "A stunning animated Awwward inspired catalog for Sore High clothing company.",
+        description: "Stunning animated Awwward inspired catalog for Sore High clothing company.",
         image: "/projects/sore_high.png",
         tags: ["Next", "TailwindCSS", "GSAP"],
         demoUrl: "https://sore-high.vercel.app/",
@@ -12,6 +12,24 @@ const projects = [
     },
     {
         id: 2,
+        title: "VolunteerConnect",
+        description: "Platform for volunteers and organisation to contribute to society wellbeing, overviewed by admin.",
+        image: "/projects/volunteer_connect.png",
+        tags: ["MERN", "EmailJS", "Cloudinary"],
+        demoUrl: "https://volunteer-connect-icip.onrender.com/",
+        gitUrl: "https://github.com/excelyynxl-a11y/volunteer-connect",
+    },
+    {
+        id: 3,
+        title: "Sapu",
+        description: "Cash-Flow Forecaster that allows user to keep track of any recurring bill or income in their account.",
+        image: "/projects/sapu.png",
+        tags: ["FARM"],
+        demoUrl: "https://sapu-y2yo.onrender.com/",
+        gitUrl: "https://github.com/excelyynxl-a11y/sapu",
+    },
+    {
+        id: 4,
         title: "JobYourLove Job Portal",
         description: "A Job-Searching and Job-Hiring web fro jobseeker and company.",
         image: "/projects/jobportal_web.png",
@@ -20,7 +38,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/job-portal-app",
     },
     {
-        id: 3,
+        id: 5,
         title: "BuyUntilImBroke EComm Web",
         description: "A complete shopping flow including cart management and secure checkout using Stripe.",
         image: "/projects/ecommerce_web.png",
@@ -29,7 +47,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/ecommerce-web",
     },
     {
-        id: 4,
+        id: 6,
         title: "Habit Tracker Mobile App",
         description: "Mobile application allows users to track daily progress, visualizes streaks and inspect habit streak rankings.",
         image: "/projects/habit_tracker_app.png",
@@ -38,7 +56,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/habit-tracker",
     },
     {
-        id: 5,
+        id: 7,
         title: "Chatty Realtime Messaging App",
         description: "A full-stack real-time messaging application built with the MERN stack. It supports instant communication with text and image messages, secure authentication, and a clean, modern UI.",
         image: "/projects/chatty_messaging_app.png",
@@ -47,7 +65,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/messaging-app",
     },
     {
-        id: 6,
+        id: 8,
         title: "KawanKawan Social Media App",
         description: "A SaaS social media platform that allows user authentication, image upload, dynamic voting and commenting system, community groups. The app is designed to be mobile responsive with modern glassmorphism aesthetic effects.",
         image: "/projects/kawan_kawan_social_media.png",
@@ -56,7 +74,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/social-media-app",
     },
     {
-        id: 7,
+        id: 9,
         title: "LikeItShort URL Shortener",
         description: "A simple and modern URL shortener that allows users to convert long URLs into short, shareable links and automatically redirects users to the original URL when accessed",
         image: "/projects/likeitshort_url_shortener.png",
@@ -65,7 +83,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/url-shortener",
     },
     {
-        id: 8,
+        id: 10,
         title: "AI Newsletter",
         description: "A SaaS platform that allows users to receive AI-generated newsletters tailored to their interests and preferred frequency.",
         image: "/projects/ai_newsletter.png",
@@ -74,7 +92,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/ai-newsletter",
     },
     {
-        id: 9,
+        id: 11,
         title: "Travel Planner",
         description: "Full-stack trip planning web app to sign in with GitHub account, create personalized travel itineraries, visualize destinations on Google Map, integrate a 3D rotating globe and a drag-and-drop itinerary planner.",
         image: "/projects/travel_planner.png",
@@ -83,7 +101,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/travel-planner-app",
     },
     {
-        id: 10,
+        id: 12,
         title: "Crypto Dashboard",
         description: "Crypto tracking web application that displays real-time cryptocurrency data, browse top cryptocurrencies, view detailed metrics, and explore a 7-day interactive chart powered by Recharts.",
         image: "/projects/crypto_dashboard.png",
@@ -92,7 +110,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/crypto-price-website",
     },
     {
-        id: 11,
+        id: 13,
         title: "Finance Tracker",
         description: "Finance Tracker with Machine Learning Predictions using Regression, Recharts, Redux Toolkit and Material UI",
         image: "/projects/finance_tracker.png",
@@ -101,7 +119,7 @@ const projects = [
         gitUrl: "https://github.com/excelyynxl-a11y/finance-tracker",
     },
     {
-        id: 12,
+        id: 14,
         title: "Weightlifting Management System",
         description: "WLMS that registers athletes, validates for registration of athletes, and determines barbell setup for a liftoff.",
         image: "/projects/wlms.png",
